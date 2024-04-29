679a0c2c126651540c6c50e88a6d6e5efde72b50
anand maurya <anandkr.maurya10@gmail.com>
2024-04-29 12:41:33 +0530

    updated

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,6 +1,27 @@
+import batase from "./images/batase.jpg";
+import burger from "./images/burger.jpg";
+import pizza from "./images/pizza.jpg";
+import icecream from "./images/icecream.jpg";
+import samosa from "./images/samosa.jpg";
+import tea from "./images/tea.jpg";
+import coffee from "./images/coffee.jpg";
+import lassi from "./images/lassi.jpg";
+import chole from "./images/chole.jpg";
+import rajma from "./images/rajma.jpg";
+import momos from "./images/momos.jpg";
+import sandwich from "./images/sandwich.jpg";
+import noddles from "./images/noddles.jpg";
+import khichadi from "./images/khichadi.jpg";
+import fruits from "./images/fruits.jpg";
+import frenchfries from "./images/frenchfries.jpg";
+import bread from "./images/bread.jpg";
+import roti from "./images/roti.jpg";
+import paneer from "./images/paneer.jpg";
+import fullthali from "./images/fullthali.jpg";
+
 const Api = [
   {
-    image:" ./images/batase.jpg",
+    image:batase,
     title: "PANI PURI",
     disc: "Pani puri is a popular street food of India, where crisp fried dough balls are stuffed with potatoes, sprouts, spicy tangy water or sweet chutney.",
     cost: "10 Rs.",
@@ -9,7 +30,7 @@ const Api = [
     category: "evening",
   },
   {
-    image: "./images/burger.jpg",
+    image: burger,
     title: "BURGER",
     disc: "A hamburger is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
     cost: "50 Rs.",
@@ -18,7 +39,7 @@ const Api = [
     category: "lunch",
   },
   {
-    image: "./images/pizza.jpg",
+    image: pizza,
     title: "PIZZA",
     disc: "Pizza is a savory dish of Italian origin , flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients.",
     cost: "100 Rs.",
@@ -27,7 +48,7 @@ const Api = [
     category: "lunch",
   },
   {
-    image: "./images/icecream.jpg",
+    image: icecream,
     title: "ICE CREAM",
     disc: "Ice cream is a sweetened frozen food typically eaten as a snack or dessert. It may be made from dairy milk or cream and is flavored with a sweetener, either sugar or an alternative.",
     cost: "20 Rs.",
@@ -36,7 +57,7 @@ const Api = [
     category: "breakfast",
   },
   {
-    image: "./images/samosa.jpg",
+    image: samosa ,
     title: "SAMOSA",
     disc: "A samosa is a fried or baked pastry with a savory filling, such as spiced potatoes, onions, peas, cheese and other ingredients. It may take different forms, including triangular, cone, or half-moon shapes",
     cost: "10 Rs.",
@@ -45,7 +66,7 @@ const Api = [
     category: "evening",
   },
   {
-    image: "./images/tea.jpg",
+    image: tea,
     title: "TEA",
     disc: "Tea is an aromatic beverage commonly prepared by pouring hot or boiling water over cured or fresh leaves of the Camellia sinensis, an evergreen shrub native to East Asia.",
     cost: "10 Rs.",
@@ -54,7 +75,7 @@ const Api = [
     category: "breakfast",
   },
   {
-    image: "./images/coffee.jpg",
+    image: coffee,
     title: "COFFEE",
     disc: "Coffee is a brewed drink prepared from roasted coffee beans, the seeds of berries from certain Coffea species. coffee berries are bright red in color ",
     cost: "20 Rs.",
@@ -63,7 +84,7 @@ const Api = [
     category: "breakfast",
   },
   {
-    image: "./images/lassi.jpg",
+    image: lassi,
     title: "LASSI",
     disc: "Lassi is a popular traditional dahi-based drink that originated in the Indian subcontinent. Lassi is a blend of yogurt, water and spices ",
     cost: "20 Rs.",
@@ -72,7 +93,7 @@ const Api = [
     category: "breakfast",
   },
   {
-    image: "./images/chole.jpg",
+    image: chole,
     title: "CHOLE CHAWAL",
     disc: "Chole bhature is a food dish popular in the Northern areas of the Indian subcontinent. Chole stands for a spiced tangy chickpea curry and bhatura is a fried leavened bread.",
     cost: "20 Rs.",
@@ -81,7 +102,7 @@ const Api = [
     category: "lunch",
   },
   {
-    image: "./images/rajma.jpg",
+    image: rajma,
     title: "RAJMA CHAWAL",
     disc: "Rajma is a popular North Indian vegetarian dish consisting of red kidney beans in a thick gravy with many Indian whole spices and usually served with rice.",
     cost: "20 Rs.",
@@ -91,7 +112,7 @@ const Api = [
   },
 
   {
-    image: "./images/momos.jpg",
+    image: momos,
     title: "MOMOS",
     disc: "Momo is a type of South Asian dumpling, popular across the Indian subcontinent and the Himalayan regions of broader South Asia.",
     cost: "20 Rs.",
@@ -100,7 +121,7 @@ const Api = [
     category: "evening",
   },
   {
-    image: "./images/sandwich.jpg",
+    image: sandwich,
     title: "SANDWICH",
     disc: "A sandwich is a food typically consisting of vegetables, sliced cheese , placed on or between slices of bread, or more generally any dish wherein bread serves as a container.",
     cost: "20 Rs.",
@@ -110,7 +131,7 @@ const Api = [
   },
 
   {
-    image: "./images/noddles.jpg",
+    image: noddles,
     title: "CHOWMEIN",
     disc: "Chow mein are Chinese stir-fried noodles with vegetables and sometimes meat or tofu; the name is a romanization of the Taishanese chāu-mèing.",
     cost: "20 Rs.",
@@ -119,7 +140,7 @@ const Api = [
     category: "evening",
   },
   {
-    image: "./images/khichadi.jpg",
+    image: khichadi,
     title: "KHICHDI",
     disc: "Khichdi is a comforting and nutritious Indian dish made from rice and lentils cooked together with aromatic spices. It's often served with yogurt, pickles, or papadum.",
     cost: "25 Rs.",
@@ -128,7 +149,7 @@ const Api = [
     category: "dinner",
   },
   {
-    image: "./images/fruits.jpg",
+    image: fruits,
     title: "FRUIT PLATTER",
     disc: "Our fruit platter features a colorful assortment of fresh, seasonal fruits, including juicy watermelon, sweet strawberries, crisp apples, succulent grapes, and more.",
     cost: "60 Rs.",
@@ -137,7 +158,7 @@ const Api = [
     category: "breakfast",
   },
   {
-    image: "./images/frenchfries.jpg",
+    image: frenchfries,
     title: "FRENCH FRIES",
     disc: "Our French fries are crispy on the outside and fluffy on the inside, perfectly seasoned with salt. .",
     cost: "25 Rs.",
@@ -146,7 +167,7 @@ const Api = [
     category: "evening",
   },
   {
-    image: "./images/bread.jpg",
+    image: bread,
     title: "BREAD",
     disc: "We offer a variety of fresh, soft, and delicious bread, including white, wheat, multigrain, and more. Perfect for sandwiches, toast, and more.",
     cost: "10 Rs.",
@@ -155,7 +176,7 @@ const Api = [
     category: "breakfast",
   },
   {
-    image: "./images/roti.jpg",
+    image: roti,
     title: "PARATHA",
     disc: "Paratha is a layered Indian flatbread made from whole wheat flour. It's flaky, crispy, and delicious, perfect for breakfast, lunch, or dinner.",
     cost: "20 Rs.",
@@ -164,7 +185,7 @@ const Api = [
     category: "dinner",
   },
   {
-    image: "./images/paneer.jpg",
+    image: paneer,
     title: "PANEER",
     disc: "Paneer is a fresh cheese common in the Indian subcontinent. It is an unaged, non-melting farmer cheese made by curdling milk with a fruit- or vegetable.",
     cost: "40 Rs.",
@@ -173,7 +194,7 @@ const Api = [
     category: "dinner",
   },
   {
-    image: "./images/fullthali.jpg",
+    image: fullthali,
     title: "INDIAN THALI",
     disc: "Our Indian thali features a variety of delicious and authentic Indian dishes, including rice, dal, sabzi, roti, raita, and more. Perfect for a complete and satisfying meal.",
     cost: "100 Rs.",
