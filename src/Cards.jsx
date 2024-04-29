2d1fc65e9c20d9a148c7195a334eb2df952a093f
anand maurya <anandkr.maurya10@gmail.com>
2024-04-29 15:35:15 +0530

    update

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -5,23 +5,23 @@ import { IoIosStar } from "react-icons/io";
 
 const Cards = ({ image, title, link1, offers, cost, disc }) => {
   return (
-    <div>
-      <div className="card" style={{ width: "22rem", margin: "30px" }}>
+    <div >
+      <div className="card card12" style={{ width: "22rem", color:"white", margin: "30px" }}>
         <img src={image} className="card-img-top imgs" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{disc}</p>
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <b>COST: &nbsp;</b> {cost}
+        <ul  className="list-group list-group-flush">
+          <li style={{color:"white"}} className="list-group-item">
+            <b >COST: &nbsp;</b> {cost}
           </li>
-          <li className="list-group-item">
+          <li style={{color:"white"}} className="list-group-item">
             <b>OFFERS: &nbsp;</b>
             {offers}
           </li>
 
-          <li className="list-group-item">
+          <li style={{color:"white"}} className="list-group-item">
             <b>RATINGS : &nbsp;</b>
             <IoIosStar /> {link1} &nbsp; read more
           </li>
